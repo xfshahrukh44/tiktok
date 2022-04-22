9c37345ee7a0501fe21a41cd7e232105704997b6
xfshahrukh44 <xixno1@gmail.com>
2022-04-23 03:50:08 +0500

    Users' email unique

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
         $validator = Validator::make($request->all(), [
             'first_name' => 'required',
             'last_name' => 'required',
-            'email' => 'required',
+            'email' => 'required|email|unique:users',
             'password' => 'required'
         ]);
 
